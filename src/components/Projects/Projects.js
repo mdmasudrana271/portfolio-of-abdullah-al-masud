b4595bb2585ed8eaa237ab176d8bb6e00137caf9
md masud rana <mr7123619@gmail.com>
2022-12-11 23:02:07 +0600

    added react photo view on project img

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Typewriter from "typewriter-effect";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -15,32 +17,43 @@ const Projects = () => {
   return (
     <section className="mt-10">
       <h2 className="typewriter text-center text-5xl font-bold pb-5">
-        
         <Typewriter
-            options={{
-              strings: ["My Project"],
-              autoStart: true,
-              loop: true,
-              delay: 75,
-            }}
-          />
+          options={{
+            strings: ["My Project"],
+            autoStart: true,
+            loop: true,
+            delay: 75,
+          }}
+        />
       </h2>
-      
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-10">
         {projects.map((project) => (
           <div key={project.id} className="card w-full bg-base-100 shadow-xl">
             <figure>
-              <img src={project.img1} alt="" />
+              <PhotoProvider>
+                <PhotoView src={project.img1}>
+                  <img src={project.img1} alt=""/>
+                </PhotoView>
+              </PhotoProvider>
             </figure>
             <div className="card-body">
               <h2 className="card-title">
                 {project.name}
-                <div className="badge badge-secondary"><a href={project?.live}>Live Link</a></div>
+                <div className="badge badge-secondary">
+                  <a href={project?.live}>Live Link</a>
+                </div>
               </h2>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline"><a href={project?.client}>Server side</a></div>
-                <div className="badge badge-outline"><a href={project?.server}>Client side</a></div>
-                <div className="badge badge-outline"><Link to={`/details/${project._id}`}>Details</Link></div>
+                <div className="badge badge-outline">
+                  <a href={project?.client}>Server side</a>
+                </div>
+                <div className="badge badge-outline">
+                  <a href={project?.server}>Client side</a>
+                </div>
+                <div className="badge badge-outline">
+                  <Link to={`/details/${project._id}`}>Details</Link>
+                </div>
               </div>
             </div>
           </div>
